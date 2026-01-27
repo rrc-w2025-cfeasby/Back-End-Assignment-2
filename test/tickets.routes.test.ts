6bdc4f7585aef7f736be14feeb2b1525e3a34217
Chris Feasby <cfeasby@academic.rrc.ca>
2026-01-27 07:48:13 -0600

    Add next test testing an invalid ticket ID returns 404 Ticket not found

--- test/tickets.routes.test.ts
+++ test/tickets.routes.test.ts
@@ -26,4 +26,12 @@ describe("Ticket Routes", () => {
         expect(response.status).toBe(200);
         expect(response.body.data.id).toBe(1);
     });
+
+    // GET /tickets/:id returns 404 for non-existing ID
+    test("GET /tickets/:id - should return 404 for non-existing ID", async () => {
+        const response = await request(app).get("/api/v1/tickets/999");
+
+        expect(response.status).toBe(404);
+        expect(response.body.message).toBe("Ticket not found");
+    });
 });
