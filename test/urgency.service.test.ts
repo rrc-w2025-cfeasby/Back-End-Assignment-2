03da6dce854d99fed152bdfbbb4da38ff40b4c33
Chris Feasby <cfeasby@academic.rrc.ca>
2026-01-27 08:18:02 -0600

    Add urgency calculation test of low urgency

---
+++ test/urgency.service.test.ts
@@ -0,0 +1,13 @@
+import { calculateTicketUrgency } from "../src/api/v1/services/ticketService";
+import { tickets } from "../src/data/tickets";
+
+describe("Ticket Urgency Calculation", () => {
+    // Test LOW urgency is calculated correctly
+    test("LOW urgency is calculated correctly", () => {
+        const result = calculateTicketUrgency(1);
+
+        if("notFound" in result) throw new Error("Ticket not found");
+
+        expect(result.urgencyLevel).toBe("Low urgency. Address when capacity allows.");
+    });
+});
